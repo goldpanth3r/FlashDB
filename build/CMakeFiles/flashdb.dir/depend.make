5237bf947f3d48a101e4e9df74bae43544ab7b91
Nagendra Bhat <50840189+goldpanth3r@users.noreply.github.com>
2026-09-17 10:20:59 +0000

    Ignore build artifacts

--- build/CMakeFiles/flashdb.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for flashdb.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/FlashDB
-# Build directory: /workspaces/FlashDB/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/workspaces/FlashDB/build/flashdb_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
